1f128d481bc760ea10b155effe0ab4612f0c5b1c
Prasanna <Prasanna@DESKTOP-EGNJR63>
2022-09-26 21:05:31 +0530

    commit2

---
+++ abstactiondiscussion/src/abstraction/Abstractionwithvariable.java
@@ -0,0 +1,12 @@
+package abstraction;
+
+public class Abstractionwithvariable {
+	
+	int i;
+	public static void main(String[] args) {
+Abstractionwithvariable av = new Abstractionwithvariable();
+		
+		System.out.println(av.i);
+	}
+
+}
---
+++ abstactiondiscussion/src/abstraction/Test.java
@@ -0,0 +1,48 @@
+package abstraction;
+
+public abstract class Test {
+	public Test()
+	{
+		System.out.println("Zero argument Test class constructor");
+	}
+	
+	
+	public void m1()
+	{
+		System.out.println("m1 method from Test class");
+	}
+	
+	public void m2()
+	{
+		System.out.println("m2 method from Test class");
+	}
+	
+	public void m3()
+	{
+		System.out.println("m3 method from Test class");
+	}
+	
+	public void m4()
+	{
+		System.out.println("m4 method from Test class");
+	}
+	
+	public abstract void m5();
+		
+	
+	
+	abstract void m6();
+	
+	public  static void m7()
+	{
+		System.out.println("m7 static method");
+	}
+
+	
+	public static void main(String[] args) {
+		
+		m7();
+		
+	}
+
+}
